832ead2cd16a9234599b8cc520ea7d8a5e17a95a
wangjunchao1118 <wangjunchao1118@gmail.com>
2017-09-22 19:47:43 +0800

    Delete make_micarray_recorder.sh

--- c/make_micarray_recorder.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# [ -f ./micarray_recorder ] && rm ./micarray_recorder
-
-g++ -c -std=c++11 -Wall -Wextra -std=gnu++11 -I ../cpp/driver ./micarray_recorder.cpp  -o ./micarray_recorder.cpp.o
-g++ ./micarray_recorder.cpp.o  -o micarray_recorder  -rdynamic  ../build/demos/driver/libmatrix_creator_hal.a -lpthread -lwiringPi -lwiringPiDev -lcrypt -lfftw3f
